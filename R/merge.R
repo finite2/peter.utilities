67e62df529dfe15896e415daa5b5a135ae20bbfe
finite2 <dutton.peter@gmail.com>
2016-08-10 08:53:36 +0100

    merge allow the db.add database to be empty
    This should allow both databases to be empty and still merge columns.
    Thus the whole pipeline should work when there is only skeleton
    data.frames. Much better than stata which will error if a dataset is
    empty.

--- R/merge.R
+++ R/merge.R
@@ -4,20 +4,26 @@
 #'
 #' Provides a function simular to merge in Stata to merge data from two data.frames.
 #'
-#' @param merge.param A vector of column names to use to merge data
+#' @param mergeParam A vector of column names to use to merge data
 #' @param db1,db2 The data.frames to merge
 #' @param entries1,entries2 1 or "m" to tell it whether to do 1:1, 1:m or m:1 matching between databases
 #' @export ut.merge
-ut.merge=function(merge.param,db1,db2,entries1,entries2){
+#'
+#' @details
+#' Merges 1:1, 1:M or M:1. M:M merging is not supported because this makes unfounded assumptions about the ordering of varaibles. It is better to specify an additional mergeParam column to merge with to explicitely asign M:M matches.
+#'
+#' Returns a column named merged denoting 1 for db1, 2 for db2 and 3 for row found in both. Note this will overwrite previous merge columns if they exist.
+#'
+ut.merge=function(mergeParam,db1,db2,entries1,entries2){
 
 
   if(entries1==1){
-    if(sum(duplicated(db1[merge.param]))>0){
+    if(sum(duplicated(db1[mergeParam]))>0){
       stop("First database entries are no uniquely identified")
     }
   }
   if(entries2==1){
-    if(sum(duplicated(db2[merge.param]))>0){
+    if(sum(duplicated(db2[mergeParam]))>0){
       stop("Second database entries are no uniquely identified")
     }
   }
@@ -25,54 +31,56 @@ ut.merge=function(merge.param,db1,db2,entries1,entries2){
 
   if(entries1=="m" | entries2==1){
     db=db1
-    db.add=db2
-    db.switch=1
+    dbAdd=db2
+    dbSwitch=1
   } else if(entries1==1 & entries2=="m") {
     db=db2
-    db.add=db1
-    db.switch=2
+    dbAdd=db1
+    dbSwitch=2
   } else {
     stop("Matching entries must be 1:1, 1:m or m:1")
   }
 
-  col.names=unique(c(colnames(db),colnames(db.add)))
-  col.names.2=colnames(db.add)
-  col.names.2.minus.intersect=col.names.2[!col.names.2 %in% intersect(colnames(db),col.names.2)]
+  colNames=unique(c(colnames(db),colnames(dbAdd)))
+  colNames2=colnames(dbAdd)
+  colNames2MinusIntersect=colNames2[!colNames2 %in% intersect(colnames(db),colNames2)]
 
 
-  classes=lapply(db.add, class)
-  db$merge=db.switch
+  classes=lapply(dbAdd, class)
+  db$merge=dbSwitch
   # add new columns as empty columns
-  for(column in col.names[!col.names %in% colnames(db)]){
+  for(column in colNames[!colNames %in% colnames(db)]){
     db[column]=NA
     class(db[[column]])=classes[[column]]
 
 
   }
 
-  db.length=dim(db)[1]
-  # merge db2 to db1
-  for(i in 1:dim(db.add)[1]){
-    match=NULL
-    for(j in 1:dim(db)[1]){
-      if(all(db.add[i,merge.param]==db[j,merge.param])){
-        match=c(match,j)
+  dbLen=dim(db)[1]
+  # merge dbAdd to db
+  if(dim(dbAdd)[1] > 0){
+    for(i in 1:dim(dbAdd)[1]){
+      match=NULL
+      for(j in 1:dim(db)[1]){
+        if(all(dbAdd[i,mergeParam]==db[j,mergeParam])){
+          match=c(match,j)
+        }
       }
-    }
-    #print(duplicated(rbind(db.add[merge.param][i,],db1[merge.param])))
-    #print(rbind(db1[merge.param],db.add[merge.param][i,]))
-    #print(match)
-    if(length(match)==0){
-      # no match found
-      db[db.length+1,]=NA
-      db$merge[db.length+1]=3-db.switch
+      #print(duplicated(rbind(dbAdd[mergeParam][i,],db1[mergeParam])))
+      #print(rbind(db1[mergeParam],dbAdd[mergeParam][i,]))
+      #print(match)
+      if(length(match)==0){
+        # no match found
+        db[dbLen+1,]=NA
+        db$merge[dbLen+1]=3-dbSwitch
 
-      db[db.length+1,col.names.2]=db.add[i,]
-      db.length=db.length+1
-    } else {
-      # match found
-      db[match,col.names.2.minus.intersect]=db.add[i,col.names.2.minus.intersect]
-      db$merge[match]=3
+        db[dbLen+1,colNames2]=dbAdd[i,]
+        dbLen=dbLen+1
+      } else {
+        # match found
+        db[match,colNames2MinusIntersect]=dbAdd[i,colNames2MinusIntersect]
+        db$merge[match]=3
+      }
     }
   }
 
