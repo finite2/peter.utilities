fbc054d9db71907a05f2ee79697e0513d8a1f672
finite2 <dutton.peter@gmail.com>
2017-03-31 11:31:17 +0100

    add blankplot to namespace

--- R/blankPlot.R
+++ R/blankPlot.R
@@ -6,6 +6,8 @@
 #' @param ylim The y axis range
 #' @param xaxs Either "i" or "r". i fits the range exactly whilst r add a little white space to either end. Default is i here which is the reverse of plot().
 #' @param yaxs Either "i" or "r". i fits the range exactly whilst r add a little white space to either end. Default is i here which is the reverse of plot().
+#'
+#' @export blankPlot
 blankPlot = function(xlim = c(0,1), ylim = c(0,1), xaxs = "i", yaxs = "i"){
   plot(0,0,axes = FALSE, xlab = "", ylab = "", xaxs = xaxs, yaxs = yaxs, type = "n")
 }
